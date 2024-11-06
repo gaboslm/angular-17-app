21b03bd97d6bae7bda51a36c61f860953b20156b
Gabriel Luna <gabriel.slmendoza@gmail.com>
2024-11-06 02:32:54 -0500

    feat: ps5 menu re-imagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Game } from '../types/game';
+import { GameListComponent } from './game-list/game-list.component';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserComponent, GameListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  country = 'Colombia'
+  cover: string | null = null
+  selectedGame: Game | null = null
+
+  setbackground(game: Game){
+    this.selectedGame = game
+    this.cover = `url(${game.cover})`
+  }
 }
---
+++ src/types/game.ts
@@ -0,0 +1,7 @@
+export interface Game {
+  id: number
+  name: string
+  color: string
+  icon: string
+  cover: string
+}
